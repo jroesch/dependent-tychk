5c5f7d7fb18286ed3131062c24703874d9ca1a92
Jared Roesch <roeschinc@gmail.com>
2015-02-28 14:16:41 -0800

    Sketch the grammar refactor

--- src/Main.hs
+++ src/Main.hs
@@ -82,7 +82,9 @@ parseTopLevel :: (Monad m, TokenParsing m) => m [Term String]
 parseTopLevel = many parseTerm
 
 parseTerm :: (Monad m, TokenParsing m) => m (Term String)
-parseTerm = Var <$> name
+parseTerm = parseType
+         <|> parseAscribe
+         <|> Var <$> name
 
 {-parseSTerm :: (Monad m, TokenParsing m) => m STerm-}
 {-parseSTerm =  Name <$> hsymbol-}
@@ -91,6 +93,33 @@ parseTerm = Var <$> name
 
 {-parseSeq :: (Monad m, TokenParsing m) => m STerm-}
 {-parseSeq = fromList <$> parseSTerm `sepBy` whiteSpace-}
+
+parseTerm0 :: (Monad m, TokenParsing m) => m (Term String)
+parseTerm0 = _hole -- everything but `t : T`
+
+parseTerm1 :: (Monad m, TokenParsing m) => m (Term String)
+parseTerm1 = _myHole -- parseTerm0 ifnot parseTerm : parseTer0 ifnot parseTerm
+
+parseTerm :: (Monad m, TokenParsing m) => m (Term String)
+parseTerm = do
+   term <- parseTerm1
+   char ':'
+   ty <- parseTerm1
+   return $ Ascribe term ty
+
+{-parseAscribe :: (Monad m, TokenParsing m) => m (Term String)-}
+{-parseAscribe = do-}
+    {-term <- parseTerm-}
+    {-rest term-}
+  {-where rest x = do-}
+          {-char ':'-}
+          {-ty <- parseTerm-}
+          {-return $ Ascribe x ty-}
+
+parseType :: (Monad m, TokenParsing m) => m (Term String)
+parseType = do
+  string "Type"
+  return Type
 
 name :: (CharParsing m) => m String
 name = some (letter <|> digit)
