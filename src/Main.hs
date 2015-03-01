7446d0bbe0f4b61628213f3a5917ff02ff7d65d5
Tristan Konolige <tristan.konolige@gmail.com>
2015-03-01 15:36:09 -0800

    added one example and repl ability to load files

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ import qualified Data.Map as M
 import Hubris.Parser
 import Hubris.Syntax
 import Hubris.TypeCheck
+import Control.Monad.Trans (lift)
 
 repl :: IO ()
 repl = runInputT defaultSettings (loop emptyContext)
@@ -16,41 +17,46 @@ repl = runInputT defaultSettings (loop emptyContext)
           loop ctxt = do
               minput <- getInputLine "hubris> "
               case minput >>= parseCommand of
+                  Just (Load file) -> do
+                    input <- lift $ readFile file
+                    outputStrLn file
+                    outputStrLn input
+                    runProgram input
+                    loop ctxt
                   Just command -> do
                       outputStrLn ("Command: " ++ show command)
                       loop ctxt
                   Nothing -> do
                       let input = fromMaybe "" minput
                       -- let result = parseString parseTopLevel (Columns 0 0) input
-                      let result = parseTerm input
-                      case result of
-                          Left doc -> do
-                              outputStrLn $ "Error! Tokens left: " ++ doc
-                              loop ctxt
-                          Right term -> do
-                              outputStrLn (show term)
-                              case typeCheckWithContext emptyContext term of
-                                Left e -> do
-                                  outputStrLn ("TypeError: " ++ (show e))
-                                  loop ctxt
-                                Right (result, ctxt') -> do
-                                  outputStrLn (show $ result)
-                                  loop ctxt'
+                      runProgram input
+                      loop ctxt
+          runProgram s = case parseTerm s of
+                           Left doc -> do
+                               outputStrLn $ "Error! Tokens left: " ++ doc
+                           Right term -> do
+                               outputStrLn (show term)
+                               case typeCheckWithContext emptyContext term of
+                                 Left e -> do
+                                   outputStrLn ("TypeError: " ++ (show e))
+                                 Right (result, ctxt') -> do
+                                   outputStrLn (show $ result)
+                              
 
 data Command = Quit
              | Help
+             | Load String
              | Unknown String
              deriving (Eq, Show)
 
 parseCommand :: String -> Maybe Command
 parseCommand (':':cmd) =
     Just $ case cmd of
-        "quit" -> Quit
-        "help" -> Help
-        s      -> Unknown s
+        "quit"                 -> Quit
+        "help"                 -> Help
+        'l':'o':'a':'d':' ':xs -> Load xs
+        s                      -> Unknown s
 parseCommand _ = Nothing
 
 main :: IO ()
 main = repl
-
-
