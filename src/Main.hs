9260a74604d2b95bac9522695c0a401acda146c3
Tristan Konolige <tristan.konolige@gmail.com>
2015-03-02 22:37:30 -0800

    added persistent history to repl

--- src/Main.hs
+++ src/Main.hs
@@ -13,33 +13,35 @@ import Hubris.Pretty
 import Control.Monad.Trans (lift)
 
 repl :: IO ()
-repl = runInputT defaultSettings (loop emptyContext)
-    where loop :: Context -> InputT IO ()
-          loop ctxt = do
-              minput <- getInputLine "hubris> "
-              case minput >>= parseCommand of
-                  Just (Load file) -> do
-                    input <- lift $ readFile file
-                    outputStrLn file
-                    outputStrLn input
-                    runProgram input
-                    loop ctxt
-                  Just command -> do
-                      outputStrLn ("Command: " ++ show command)
-                      loop ctxt
-                  Nothing -> do
-                      runProgram $ fromMaybe "" minput
-                      loop ctxt
-          runProgram s = case parseTerm s of
-                           Left doc -> do
-                               outputStrLn $ "Error! Tokens left: " ++ doc
-                           Right term -> do
-                               outputStrLn (show term)
-                               case typeCheckWithContext emptyContext term of
-                                 Left e -> do
-                                   outputStrLn ("TypeError: " ++ (show e))
-                                 Right (result, ctxt') -> do
-                                   outputStrLn (show $ result)
+repl = runInputT settings (loop emptyContext)
+    where
+      settings = defaultSettings {historyFile = Just ".hubris_history"}
+      loop :: Context -> InputT IO ()
+      loop ctxt = do
+          minput <- getInputLine "hubris> "
+          case minput >>= parseCommand of
+              Just (Load file) -> do
+                input <- lift $ readFile file
+                outputStrLn file
+                outputStrLn input
+                runProgram input
+                loop ctxt
+              Just command -> do
+                  outputStrLn ("Command: " ++ show command)
+                  loop ctxt
+              Nothing -> do
+                  runProgram $ fromMaybe "" minput
+                  loop ctxt
+      runProgram s = case parseTerm s of
+                       Left doc -> do
+                           outputStrLn $ "Error! Tokens left: " ++ doc
+                       Right term -> do
+                           outputStrLn $ "Parsed: " ++ show term
+                           case typeCheckWithContext emptyContext term of
+                             Left e -> do
+                               outputStrLn ("TypeError: " ++ (show e))
+                             Right (result, ctxt') -> do
+                               outputStrLn $ "Typecheck: " ++ show result
                               
 
 data Command = Quit
